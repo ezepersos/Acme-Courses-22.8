f04a0d180b7174932d8da7d469cd2af2f013677f
ezepersos <ezequielperez50@gmail.com>
2022-11-13 11:31:45 +0100

    Task-046: Done

---
+++ src/main/java/acme/features/administrator/dashboard/AdministratorDashboardRepository.java
@@ -0,0 +1,23 @@
+package acme.features.administrator.dashboard;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+
+import acme.entities.help_requests.HelpRequestStatus;
+import acme.entities.tutorials.TutorialType;
+import acme.framework.repositories.AbstractRepository;
+
+
+public interface AdministratorDashboardRepository extends AbstractRepository{
+	
+	@Query("select count(t) from Tutorial t where t.tutorialType = :type")
+	Integer totalTutorials(TutorialType type);
+	
+	@Query("select t.cost.currency, avg(t.cost.amount), stddev(t.cost.amount),  min(t.cost.amount), max(t.cost.amount) from Tutorial t group by t.cost.currency")
+	List<Object[]> operationsTutorialsByC(TutorialType type);
+
+	@Query("select count(h), avg(h.budget.amount), stddev(h.budget.amount), min(h.budget.amount), max(h.budget.amount) from HelpRequest h where h.status = :status")
+	List<Object[]> operationsHelpRequest(HelpRequestStatus status);
+	
+}
